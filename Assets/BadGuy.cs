5b93d11b7bbd11bf93a0ff3edd5218bc918eead6
Zhamanta Chavez <zhamantach04@gmail.com>
2025-04-11 09:11:26 -0500

    Working on Wander State

--- Assets/BadGuy.cs
+++ Assets/BadGuy.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,32 @@ using UnityEngine.AI;
 public class BadGuy : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agent;
+
+    [SerializeField] Transform wander1;
+    [SerializeField] Transform wander2;
+
     [SerializeField] Transform target;
+
+    [SerializeField] MyStates m_States;
+
+    float wanderRange;
+    Vector3 startingLocation;
+    float playerSightRange;
+    float playerAttackRange;
+    float currentStateElapsed;
+    float recoveryTime;
+
+
+    float distance1;
+    float distance2;
+
+    enum MyStates
+    { 
+        WANDER,
+        PURSUE,
+        ATTACK,
+        RECOVERY
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +43,71 @@ public class BadGuy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerSightRange = Vector3.Distance(transform.position, target.position);
+
+        switch(m_States)
+        {
+            case MyStates.WANDER:
+                UpdateWander();
+                break;
+            case MyStates.PURSUE:
+                UpdatePursue();
+                break;
+            case MyStates.ATTACK:
+                UpdateAttack();
+                break;
+            case MyStates.RECOVERY:
+                UpdateRecovery();
+                break;
+        }
+    }
+
+    void UpdateWander()
+    {
+        if (agent.SetDestination(wander1.position) == true)
+        {
+                if (distance1 < 1f)
+                {
+                    agent.SetDestination(wander2.position);
+                    distance2 = Vector3.Distance(transform.position, wander2.position);
+                }
+        }
+
+        if (agent.SetDestination(wander2.position) == true)
+        {
+                if (distance2 < 1f)
+                {
+                    agent.SetDestination(wander1.position);
+                    distance1 = Vector3.Distance(transform.position, wander2.position);
+                }
+        }
+    }
+
+    void UpdatePursue()
+    {
         agent.SetDestination(target.position);
+
+        if (playerSightRange >= 5f)
+        {
+            agent.SetDestination(wander1.position);
+            m_States = MyStates.WANDER;
+        }
+
+        /*if (playerSightRange <= 5f)
+        {
+            Debug.Log("Bad Guy has attacked");
+            m_States = MyStates.;
+        }*/
+
+    }
+
+    void UpdateAttack()
+    {
+        Debug.Log("Enemy has attacked!");
+    }
+
+    void UpdateRecovery()
+    {
+
     }
 }

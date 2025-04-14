4cff753f36955d416a27d1d0e51be668c0362851
Zhamanta Chavez <zhamantach04@gmail.com>
2025-04-14 12:22:59 -0500

    Wander State Done

--- Assets/BadGuy.cs
+++ Assets/BadGuy.cs
@@ -8,9 +8,6 @@ public class BadGuy : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agent;
 
-    [SerializeField] Transform wander1;
-    [SerializeField] Transform wander2;
-
     [SerializeField] Transform target;
 
     [SerializeField] MyStates m_States;
@@ -22,15 +19,8 @@ public class BadGuy : MonoBehaviour
     float currentStateElapsed;
     float recoveryTime;
 
-
-    [SerializeField] Transform wander3;
     [SerializeField] Transform wander4;
-
-    float distance3;
     float distance4;
-
-    float distance1;
-    float distance2;
 
     enum MyStates
     { 
@@ -43,11 +33,7 @@ public class BadGuy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        wander3.position = new Vector3(Random.Range(-7.5f, 18), -.27f, Random.Range(15, 48));
-
-        wander4.position = new Vector3(Random.Range(-7.5f, 18), -.27f, Random.Range(15, 48));
-
-        agent.SetDestination(wander3.position);
+ 
 
     }
 
@@ -75,45 +61,11 @@ public class BadGuy : MonoBehaviour
 
     void UpdateWander()
     {
-        /*if (agent.SetDestination(wander1.position) == true)
-        {
-            if (distance1 < 1f)
-            {
-                agent.SetDestination(wander2.position);
-                distance2 = Vector3.Distance(transform.position, wander2.position);
-            }
-        }
-
-        if (agent.SetDestination(wander2.position) == true)
-        {
-            if (distance2 < 1f)
-            {
-                 agent.SetDestination(wander1.position);
-                 distance1 = Vector3.Distance(transform.position, wander1.position);
-            }
-        }*/
-
-        /*if (agent.SetDestination(wander3.position) == true)
-        {
-            distance3 = Vector3.Distance(transform.position, wander3.position);
-            if (distance3 < 1f)
-            {
-                agent.SetDestination(wander4.position);
-                Debug.Log("4: " + agent.SetDestination(wander4.position));
-                distance4 = Vector3.Distance(transform.position, wander4.position);
-                wander3.position = new Vector3(Random.Range(-7.5f, 18), -.27f, Random.Range(15, 48));
-            }
-        }*/
-
         if (agent.SetDestination(wander4.position) == true)
         {
-            Debug.Log("4: " + agent.SetDestination(wander4.position));
             distance4 = Vector3.Distance(transform.position, wander4.position);
             if (distance4 < 1f)
             {
-                agent.SetDestination(wander3.position);
-                Debug.Log("3: " + agent.SetDestination(wander4.position));
-                distance3 = Vector3.Distance(transform.position, wander3.position);
                 wander4.position = new Vector3(Random.Range(-7.5f, 18), -.27f, Random.Range(15, 48));
             }
         }
